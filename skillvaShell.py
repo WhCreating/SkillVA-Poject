bc8203b3a9fb99ce0058aa8c75edb1019f7d8290
LooooZ <162182067+WhCreating@users.noreply.github.com>
2024-10-13 20:43:56 +0500

    Made some changes

--- skillvaShell.py
+++ skillvaShell.py
@@ -3,7 +3,17 @@ import synthesis
 import voicer
 import asyncio
 
-while True:
-    b = asyncio.run(llama.chat(voicer.recording('C:\skillva\skillva\models\small_model_ru', 2)))
-    synthesis.synthes(b)
-    print(b)
+def shell(model='C:\skillva\skillva\models\model_ru', micros=2):  
+    while True:
+        c = voicer.recording(model, micros)
+        print(c)
+        if "бот" in c:
+            b = asyncio.run(llama.chat(c))
+            synthesis.synthes(b)
+            print(b)
+        else :
+            synthesis.synthes("Укажите голосовую команду!")
+    
+    
+if __name__ == '__main__':
+    shell()
